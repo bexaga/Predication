21aaef8ce2329b9a08c7dec07e9a20441463e4da
bexaga <136268170+bexaga@users.noreply.github.com>
2024-12-18 11:45:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if st.button("Proposer un thème"):
                         st.info("Aucune option sélectionnée.")
                 else:
                     st.error("La réponse ne contient pas la structure attendue 'options'.")
-            st.write(f"Thème: {response_content['options'][index]}")
+                st.write(f"Thème: {response_content['options'][index]}")
  
 
             except json.JSONDecodeError:
